5ab09f42bfd0c4f8c6aaec0ab4a9732e4d454b19
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-01-07 01:05:22 +0900

    とりあえずURLはハードコーディング

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const BaseURL = import.meta.env.VITE_NODE_ENV
-  ? 'https://docker-knex-postgres.onrender.com/data'
-  : 'http://localhost:6100/data';
-console.log('VITE_NODE_ENV : ', import.meta.env.VITE_NODE_ENV);
+// const BaseURL = import.meta.env.VITE_NODE_ENV
+//   ? 'https://docker-knex-postgres.onrender.com/data'
+//   : 'http://localhost:6100/data';
+const BaseURL = 'https://docker-knex-postgres.onrender.com/data';
 console.log('BaseURL : ', BaseURL);
 
 console.log('BACKEND_DOMAIN : ', import.meta.env.BACKEND_DOMAIN);
