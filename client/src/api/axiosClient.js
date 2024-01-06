3f1714f1308524268207e40dbf8e893562096bcd
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-01-07 00:47:13 +0900

    NODE_VITE_ENVも表示

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const BaseURL = import.meta.env.VITE_NODE_ENV
   ? 'https://docker-knex-postgres.onrender.com/data'
   : 'http://localhost:6100/data';
+console.log('VITE_NODE_ENV : ', import.meta.env.VITE_NODE_ENV);
 console.log('BaseURL : ', BaseURL);
 
 console.log('BACKEND_DOMAIN : ', import.meta.env.BACKEND_DOMAIN);
